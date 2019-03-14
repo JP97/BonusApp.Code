1b0b74859086971e83134dd0f67f31e0688047f0
Julian <juli11c0@edu.eal.dk>
2019-03-14 09:12:03 +0100

    Add project files.

---
+++ BonusApp/Bonuses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public delegate double BonusProvider(double amount);
+
+    public class Bonuses
+    {
+		public static BonusProvider TenPercent = amount => amount / 10;
+
+		public static BonusProvider FlatTwoIfAmountMoreThanFive = amount => amount > 5.0 ? 2.0 : 0.0;
+    }
+}
---
+++ BonusApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    class Program
+    {
+		public delegate int AnonymeLambdaDelegates(int x);
+		public delegate int AnonymeLambdaDelegates2Parameters(int x, int y);
+		static void Main(string[] args)
+        {
+			AnonymeLambdaDelegates2Parameters d = (x, y) => { return x + y; };
+
+			Console.WriteLine("das ist der mit 2 argumenten: " + d(5,5));
+
+			AnonymeLambdaDelegates d1 = x => x * 10;
+
+			Console.WriteLine("das ist der eine der mal 10 macht: " + d1(5));
+
+			d1 = x => x * x;
+
+			Console.WriteLine("das ist der eine der normal mal macht: " + d1(5));
+
+			Console.ReadKey();
+
+		}
+    }
+}
